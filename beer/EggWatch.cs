6be23d628f74d7cc8b64d01116de17345fafdf8b
christian johansen <cj.post@gmail.com>
2017-12-08 08:17:30 +0100

    timer functionallity in seperate class

---
+++ beer/EggWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace beer
+{
+    public class EggWatch
+    {
+        int minutes;
+        int seconds;
+        string function = "";
+
+        public EggWatch()
+        {
+        }
+
+        public static void hejsa() {
+            
+        }
+
+        public override string ToString()
+        {
+            return "" + minutes + ":" + seconds + " " + function;
+        }
+    }
+}
